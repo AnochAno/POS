3531f04e8c7476ba179777c22b1bacf0c33cdcff
AnochAno <anothankes876@gmail.com>
2023-12-10 09:11:36 +0530

    pup

--- System/Adminboard/product.cs
+++ System/Adminboard/product.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace System.Adminboard
 {
-    public partial class Form1 : Form
+    public partial class product : Form
     {
-        public Form1()
+        public product()
         {
             InitializeComponent();
         }
