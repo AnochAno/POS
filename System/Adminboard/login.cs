ea80e61886f5a1924a134d4391b66892892f4089
AnochAno <anothankes876@gmail.com>
2023-12-30 14:50:00 +0530

    delete login code

--- System/Adminboard/login.cs
+++ System/Adminboard/login.cs
@@ -15,52 +15,13 @@ namespace System.Adminboard
 {
     public partial class login : Form
     {
-        public login()
-        {
-            InitializeComponent();
-            //pass.UseSystemPasswordChar = true;
-        }
-
-        SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-MM7BIH0F;Initial Catalog=System;Integrated Security=True");
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
-            String Username, Password;
+            
 
-            Username = name.Text;
-            Password = pass .Text;
-
-            try
-            {
-                String Save = "SELECT * FROM Admin_Registration WHERE UserName = '" +name.Text+ "' AND Password = '" +pass.Text+ "' ";
-                SqlDataAdapter sda = new SqlDataAdapter(Save, conn);
-                DataTable dt = new DataTable();
-                sda.Fill(dt);
-
-                if(dt.Rows.Count > 0)
-                {
-                    Username = name.Text;
-                    Password = pass .Text;
-
-                    aadmin ad = new aadmin();
-                    ad.Show();
-                    this.Hide();
-
-                }
-                else
-                {
-                    MessageBox.Show("UserName and Password Not Matched");
-                    clear();
-                }
-
-
-            }catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message + "Error in Your Database connection" , "Error " , MessageBoxButtons.OK);
-                clear();
-            }finally { 
-            conn.Close();
-            }
+           
 
         }
 
@@ -88,7 +49,7 @@ namespace System.Adminboard
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            //pass.UseSystemPasswordChar = !checkBox1.Checked;
+            
         }
     }
 }
