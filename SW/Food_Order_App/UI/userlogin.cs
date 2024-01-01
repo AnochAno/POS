e2a1c7b25c884e7f28a16312ed86903055c8c96b
AnochAno <anothankes876@gmail.com>
2024-01-01 11:45:39 +0530

    login.user Code

--- SW/Food_Order_App/UI/userlogin.cs
+++ SW/Food_Order_App/UI/userlogin.cs
@@ -16,5 +16,42 @@ namespace Food_Order_App.UI
         {
             InitializeComponent();
         }
+
+        public void clear()
+        {
+            txtusername.Clear();
+            txtpass.Clear();
+            txtusername.Focus();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncan_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void showpass_CheckedChanged(object sender, EventArgs e)
+        {
+            txtpass.UseSystemPasswordChar = !showpass.Checked;
+        }
     }
 }
